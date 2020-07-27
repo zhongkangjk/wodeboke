2cda3602e7ca8dc2cfb157188640591a7d639b6f
zhongkangjk <296053096@qq.com>
2020-07-27 22:12:42 +0800

    模板规划完成

--- 我的博客/urls.py
+++ 我的博客/urls.py
@@ -18,8 +18,10 @@ from django.urls import path
 #导几个包
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='home.html')),
     path('admin/', admin.site.urls),
 ]
 
